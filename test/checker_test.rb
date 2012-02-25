7474333a587f4ecb582cd31eb1b71dd43170d6e7
coutud <wam@atwam.com>
2012-02-25 14:39:05 +0000

    Reput checker code, needed in tests

--- test/checker_test.rb
+++ test/checker_test.rb
@@ -4,17 +4,20 @@ require 'merimee'
 require 'digest/sha1'
 
 class CheckerTest < MiniTest::Unit::TestCase
+  ATD_SLEEP = 0.7
   def setup
     @checker = Merimee::Checker.new
   end
 
   def test_check_perfect_text
+    sleep ATD_SLEEP # Make sure we sleep a bit, AtD returns 503 if requests are too close
     errors = @checker.check("Perfect text")
     assert_empty errors
   end
 
   def test_text_with_errors
     erroneous_text = "Wrong text garanteed with errors"
+    sleep ATD_SLEEP # Make sure we sleep a bit, AtD returns 503 if requests are too close
     errors = @checker.check(erroneous_text)
 
     refute_empty errors
